a3a63b00af498096d87f52e8298808a9b672d7e7
wagnertrindades <wagner.sanseverin@resultadosdigitais.com.br>
2017-06-04 13:27:42 -0300

    Add package lesson

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/wagnertrindades/MyGolangTraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+	fmt.Println(stringutil.MyName)
+}
